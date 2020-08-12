c76074f549837770a098d63738cb6e6016a07dd6
Heath Kelley <heathkelley230@gmail.com>
2020-08-12 18:18:26 -0500

    added js

--- js/app.js
+++ js/app.js
@@ -14,4 +14,4 @@ if(name === 'yes'){
 } else {
   alert('Something went wrong.  Please refresh the page and select "yes or no"');
 }
-document.getElementById('discount-placeholder').textContent = welcome <h4 style=color:red></h4>;
+document.getElementById('discount-placeholder').textContent = welcome;
