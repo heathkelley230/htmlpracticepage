94e51788bfc970e5cf3536790eda875bfe849602
Heath Kelley <heathkelley230@gmail.com>
2020-08-12 14:27:10 -0500

    updated js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 'use strict';
-console.log('this file is connected and ready to run');
 
 var name = prompt('Are you a veteran or currently serving in the armed forces?');
 
 // variable declaration for the response to being a veteran
+
 var welcome;
-if(name = yes = yes){welcome = 'Thank you for your service.  Please enjoy a 20% discount for any items purchased using the code "truepatriot".';
-} else if (name = Yes){welcome = 'Thank you for your service.  Please enjoy a 20% discount for any items purchased using the code "truepatriot".';
-} else if (name = no){welcome = 'We contribute 20% of all purchases to the Wounded Warrior Association.';
-} else if (name = No){welcome = 'We contribute 20% of all purchases to the Wounded Warrior Association.';
-} else {'Something went wrong.  Please select "yes or no"';
+if(name === 'yes'){
+  welcome = 'Thank you for your service.  Please enjoy a 20% discount for any items purchased using the code "truepatriot".';
+} else if (name === 'no'){
+  welcome = 'We contribute 20% of all purchases to the Wounded Warrior Association.';
+} else {
+  welcome = 'Something went wrong.  Please select "yes or no"';
 }
+console.log(welcome);
