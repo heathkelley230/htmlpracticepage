566b6555460f11c61f39aece866eb05fadcacbf6
Heath Kelley <heathkelley230@gmail.com>
2020-08-12 17:15:58 -0500

    made background white

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 'use strict';
 console.log('ready');
-var name = prompt('Are you a veteran or currently serving in the armed forces?');.toLowerCase().trim();
-
+var name = prompt('Are you a veteran or currently serving in the armed forces?').toLowerCase().trim();
 // variable declaration for the response to being a veteran
-
 var welcome;
 if(name === 'yes'){
   welcome = 'Thank you for your service.  Please enjoy a 20% discount for any items purchased using the code "truepatriot".';
