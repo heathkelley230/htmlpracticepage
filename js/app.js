faf95726acaeae5653ae171ca2786e066c6365b9
Heath Kelley <heathkelley230@gmail.com>
2020-08-12 17:52:37 -0500

    added name input js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 'use strict';
 console.log('ready');
+
+var name = window.prompt("Enter your name: ");
+alert('Hello ' + name + '!' + ' Welcome to Patriot Woodworking!');
+
 var name = prompt('Are you a veteran or currently serving in the armed forces?').toLowerCase().trim();
 // variable declaration for the response to being a veteran
 var welcome;
@@ -8,6 +12,6 @@ if(name === 'yes'){
 } else if (name === 'no'){
   welcome = 'We contribute 20% of all purchases to the Wounded Warrior Association.';
 } else {
-  welcome = 'Something went wrong.  Please refresh the page and select "yes or no"';
+  alert('Something went wrong.  Please refresh the page and select "yes or no"');
 }
 document.getElementById('discount-placeholder').textContent = welcome;
