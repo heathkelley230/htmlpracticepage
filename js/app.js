1c16e8dfdce8975f23230067938240890d24d1a5
Heath Kelley <heathkelley230@gmail.com>
2020-08-12 19:38:00 -0500

    added space in js string

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ var name = prompt('Are you a veteran or currently serving in the armed forces?')
 // variable declaration for the response to being a veteran
 var welcome;
 if(name === 'yes'){
-  welcome = 'Thank you for your service' + person + '.' + 'Please enjoy a 20% discount for any items purchased using the code "truepatriot".';
+  welcome = 'Thank you for your service' + person + '.' + ' Please enjoy a 20% discount for any items purchased using the code "truepatriot".';
 } else if (name === 'no'){
   welcome = 'Thank you for viewing our Website.  We contribute 20% of all purchases to the Wounded Warrior Association.';
 } else {
