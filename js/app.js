45b4e5e5a70450b0f17d98e436895ecd91b8c878
Heath Kelley <heathkelley230@gmail.com>
2020-08-13 15:10:33 -0500

    updated js

--- js/app.js
+++ js/app.js
@@ -3,16 +3,12 @@ console.log('ready');
 
 var person = window.prompt("Enter your name: ");
 alert('Hello ' + person + '!' + ' Welcome to Patriot Woodworking!');
-
 var name = prompt('Are you a veteran or currently serving in the armed forces?').toLowerCase().trim();
-// variable declaration for the response to being a veteran
 var welcome;
-var years; 
-
 if(name === 'yes'){
   welcome = 'Thank you for your service' + person + '.';
 } if(name === 'yes'){
-  welcome = prompt(person + ', how many total years of active duty have you served?');
+  var years = prompt(person + ', how many total years of active duty have you served?');
 } else if(years > 10){'Thank you for your service.  Enjoy 20% off of all merchandise.';
 } else if((years > 5) && (years < 11)){'Thank you for your service.  Enjoy 15% off of all merchandise.'; 
 } else if (years < 6){'Thank you for your service.  Enjoy 10% off of all merchandise.';
@@ -21,5 +17,4 @@ if(name === 'yes'){
 } else {
   alert('Something went wrong.  Please refresh the page and select "yes or no"');
 }
-
 document.getElementById('discount-placeholder').textContent = welcome;
