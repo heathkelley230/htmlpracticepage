24f9ba0e9f412fb37ec61c8730662baa1049ad9a
Heath Kelley <heathkelley230@gmail.com>
2020-08-13 18:11:19 -0500

    trying to separate else if statements

--- js/app.js
+++ js/app.js
@@ -7,19 +7,18 @@ var name = prompt('Are you a veteran or currently serving in the armed forces?')
 var welcome;
 if(name === 'yes'){
   welcome = 'Thank you for your service' + person + '.';
-} if(name === 'yes'){
-  var years = prompt(person + ', how many total years of active duty have you served?');
-} else if(years > 10){'Thank you for your service.  Enjoy 20% off of all merchandise.';
-} else if((years > 5) && (years < 11)){'Thank you for your service.  Enjoy 15% off of all merchandise.'; 
-} else if (years < 6){'Thank you for your service.  Enjoy 10% off of all merchandise.';
 } 
-
-document.getElementById('discount-placeholder').textContent = years;
-
-
 else if (name === 'no'){
   welcome = 'Thank you for viewing our Website.  We contribute 20% of all purchases to the Wounded Warrior Association.';
 } else {
   alert('Something went wrong.  Please refresh the page and select "yes or no"');
 }
 document.getElementById('discount-placeholder').textContent = welcome;
+
+var years = prompt(person + ', how many total years of active duty have you served?');
+} else if(years > 10){'Thank you for your service.  Enjoy 20% off of all merchandise.';
+} else if((years > 5) && (years < 11)){'Thank you for your service.  Enjoy 15% off of all merchandise.'; 
+} else if (years < 6){'Thank you for your service.  Enjoy 10% off of all merchandise.';
+} 
+
+document.getElementById('yearsdiscount').textContent = years;
