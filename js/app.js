a3f75b4a5d4cc804a5f568d40e307a51dc16a1de
Heath Kelley <heathkelley230@gmail.com>
2020-08-14 04:38:42 -0500

    updated js after feedback from instructor, decided to add back code about years of service.

--- js/app.js
+++ js/app.js
@@ -1,32 +1,23 @@
 'use strict';
- 
+
 var person = window.prompt("Enter your name: ");
 alert('Hello ' + person + '!' + ' Welcome to Patriot Woodworking!');
- 
 var name = prompt('Are you a veteran or currently serving in the armed forces?').toLowerCase().trim();
-
 var welcome;
- 
 if(name === 'yes'){
- welcome = alert('Thank you for your service ' + person + '.' + ' Please enjoy a 20% discount for any items purchased using the code "truepatriot".');
-} else if (name === 'no'){
- welcome = alert('Thank you for viewing our Website.  We contribute 20% of all purchases to the Wounded Warrior Association.');
+  welcome = 'Thank you for your service ' + person + '. ';
+  var years = prompt(person + ', how many total years of active duty have you served?');
+  if(years >= 10){
+    welcome += 'Enjoy 20% off of all merchandise using the code "truepatriot" at checkout.';
+  } else if(years >= 5){
+    welcome += 'Enjoy 15% off of all merchandise using the code "eaglenest" at checkout.'; 
+  } else {
+    welcome += 'Enjoy 10% off of all merchandise.';
+  } 
+} 
+else if (name === 'no'){
+  welcome = 'Thank you for viewing our Website.  We contribute 10% of all purchases to the Wounded Warrior Association.';
 } else {
- alert('Something went wrong.  Please refresh the page and select "yes or no"');
+  alert('Something went wrong.  Please refresh the page and select "yes or no".');
 }
-
-var donation = prompt('Would you consider donating to the Wounded Warrior Association?').toLowerCase().trim();
-
-var woundedwarrior;
- 
-if(donation === 'yes'){
- woundedwarrior = 'Thank you for your generosity ' + person + '.' + ' Please select the donation dropdown to add your donation amount at checkout.';
-} else if (donation === 'no'){
- woundedwarrior = 'Thank you for viewing our Website.  We contribute 20% of all purchases to the Wounded Warrior Association.';
-} else {
- alert('Something went wrong.  Please refresh the page and try again.');
-}
-
-document.getElementById('discount-placeholder').textContent = woundedwarrior;
-
-
+document.getElementById('discount-placeholder').textContent = welcome;
