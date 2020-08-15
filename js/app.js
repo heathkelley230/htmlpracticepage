f1ec7d925fd6e33b5c74d90e96901f866b9af0f8
Heath Kelley <heathkelley230@gmail.com>
2020-08-15 08:40:24 -0500

    updated page to add form and error validation js

--- js/app.js
+++ js/app.js
@@ -21,3 +21,39 @@ else if (name === 'no'){
   alert('Something went wrong.  Please refresh the page and select "yes or no".');
 }
 document.getElementById('discount-placeholder').textContent = welcome;
+
+//Begin Form 
+
+  
+function validate(){
+  var name = document.getElementById("name").value;
+  var phone = document.getElementById("phone").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  var error_message = document.getElementById("error_message");
+  
+  error_message.style.padding = "10px";
+  
+  var text;
+  if(name.length < 5){
+    text = "Please Enter valid Name";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if(isNaN(phone) || phone.length != 10){
+    text = "Please Enter valid Phone Number";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if(email.indexOf("@") == -1 || email.length < 6){
+    text = "Please Enter valid Email";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if(message.length >= 400){
+    text = "Please Enter Less Than 400 Characters";
+    error_message.innerHTML = text;
+    return false;
+  }
+  alert("Form Submitted Successfully!");
+}
